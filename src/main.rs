39363e4c0316b13d42a357f9035303200c5bbc9c
Xynnn007 <278954852@qq.com>
2021-12-27 14:37:34 +0800

    feat: simple async handle
    Signed-off-by: Xynnn007 <278954852@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use std::{net::{TcpListener, TcpStream}, fs, io::{Write, Read}};
+use std::{fs, time::Duration};
+use std::error::Error;
+use async_std::io::WriteExt;
+use async_std::net::{TcpListener, TcpStream};
+use async_std::task::spawn;
+use futures::{AsyncReadExt, StreamExt};
+use log::{LevelFilter, info};
 
-fn handle_connection(mut stream: TcpStream) {
+const ADDRESS : &str = "0.0.0.0:7878";
+
+async fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0;1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "html/hello.html")
+    } else if buffer.starts_with(sleep) {
+        async_std::task::sleep(Duration::from_secs(3)).await;
         ("HTTP/1.1 200 OK\r\n\r\n", "html/hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "html/404.html")
@@ -13,14 +25,24 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string(filename).unwrap();
 
     let response = format!("{}{}", status_line, contents);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
-fn main() {
-    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
-    }
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>>{
+    env_logger::builder().filter_level(LevelFilter::Info).init();
+    
+    let listener = TcpListener::bind(ADDRESS).await?;
+    info!("Start listening {}...", ADDRESS);
+    listener
+        .incoming()
+        .for_each_concurrent(100, |stream|async move {
+            let stream = stream.unwrap();
+            info!("Got new connection from {}", stream.peer_addr().unwrap().to_string());
+            spawn(handle_connection(stream));
+    })
+    .await;
+    
+    Ok(())
 }
